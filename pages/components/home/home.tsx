1b4c3c3ad9475c7d2bfb1725ddd18e9810acbe39
Alan Franzin <rafael.thomazelli@gmail.com>
2023-11-14 21:23:42 -0300

    Update home.tsx

--- pages/components/home/home.tsx
+++ pages/components/home/home.tsx
@@ -10,7 +10,7 @@ const Home: React.FC = () => {
           <Col lg={6}>
             <div className="banner-content">
               <h1 className="mb-30 text-uppercase" data-aos="fade-up" data-aos-duration="400" data-aos-delay="400" data-aos-easing="ease-out-cubic">Rafael Thomazelli</h1>
-              <p className="lead mb-30" data-aos="fade-up" data-aos-duration="400" data-aos-delay="600" data-aos-easing="ease-out-cubic">Diretor de tecnologia Devaria, Staart e Verzel que ama ensinar programação para pessoas.</p>
+              <p className="lead mb-30" data-aos="fade-up" data-aos-duration="400" data-aos-delay="600" data-aos-easing="ease-out-cubic">Diretor de tecnologia Devaria, Staart e Verzel que ama ensinar programação para pessoas!!</p>
               <div className="socials-icons socials-grey" data-aos="fade-up" data-aos-duration="400" data-aos-delay="800" data-aos-easing="ease-out-cubic">
                 <NextLink href={"https://www.instagram.com/rafamazzucato"} >
                   <a rel="noopener noreferrer" target="_blank">
